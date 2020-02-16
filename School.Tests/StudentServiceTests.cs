a5777a7560de54faab6480bf28f9bcbb2cbe7d7c
Habib Ullah <hhaabiib@hotmail.com>
2020-02-16 22:49:19 +0000

    registration improvements

---
+++ School.Tests/StudentServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using NSubstitute;
+using School.Services;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace School.Tests
+{
+    public class StudentServiceTests
+    {
+    }
+}
